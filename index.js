1c6d029dc5bc8816cc10a663cc25c9f6ea071684
satvik1311 <satvik.gaur12@gmail.com>
2026-01-15 16:48:23 +0530

    /

--- index.js
+++ index.js
@@ -195,6 +195,7 @@ server.listen(8000, () => {
 });
 */
 
+/*
 const http = require("http");
 const { logActivity, showLogs } = require("./logger");
 
@@ -242,4 +243,136 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, () => {
     console.log("Server is listening on port 8000");
+});
+*/
+/*
+// jab bhi server pe request ayegi uska log kese generate hoga 
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const timestamp = new Date().toLocaleString();
+
+  const log = `User requested at: ${timestamp} , Request URL: ${req.url}\n`;
+
+  fs.appendFile("./activity.log", log, (err) => {
+    if (err) {
+      console.log("Fail to write log");
+    } else {
+      console.log("Log written successfully");
+    }
+  });
+  switch (req.url) {
+        case "/":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end("<h1>Welcome to Home Page</h1>");
+            break;
+
+        case "/about":
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end("<h1>About Us Page</h1>");
+            break;
+
+        case "/contact":
+            const user = {
+                id: 1,
+                name: "Satvik",
+                contact: "7080809670"
+            };
+
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(
+                "<h1>Contact Us</h1>" +
+                "<p>Contact: " + user.contact + "</p>"
+            );
+            break;
+            case "/alllogs":
+                fs.readFile("./activity.log", "utf-8", (err, data) => {
+                    if (err) {
+                        res.writeHead(200, { "Content-Type": "text/plain" });
+                        res.end("No logs found");
+                        return;     
+                    }
+                    else {
+                        res.writeHead(200, { "Content-Type": "text/plain" });
+                        res.end(data);
+                    }
+                });
+                break;
+        
+
+        default:
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end("<h1>404 Page Not Found</h1>");
+            break;
+    }
+
+  
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+*/
+
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const { name, email } = parsedUrl.query;
+
+  switch (pathname) {
+
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1>Welcome to Home Page</h1>");
+      break;
+
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(
+        `<h1>About Us</h1>
+         <p>Hello I am ${name || "Guest"}</p>
+         <p>Email: ${email || "Not provided"}</p>`
+      );
+      break;
+
+    case "/contact":
+      const user = {
+        id: 1,
+        name: "Satvik",
+        contact: "7080809670"
+      };
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(
+        `<h1>Contact Us</h1>
+         <p>Contact: ${user.contact}</p>`
+      );
+      break;
+
+    case "/alllogs":
+      fs.readFile("./activity.log", "utf-8", (err, data) => {
+        if (err) {
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.end("No logs found");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.end(data);
+        }
+      });
+      break;
+
+    default:
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 Page Not Found</h1>");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
