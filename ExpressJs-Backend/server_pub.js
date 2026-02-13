1b8105263e62a39070141a744e92439880b3772f
satvik1311 <satvik.gaur12@gmail.com>
2026-02-13 16:53:53 +0530

    feat: implement student registration and display functionality with EJS rendering

--- ExpressJs-Backend/server_pub.js
+++ ExpressJs-Backend/server_pub.js
@@ -13,52 +13,54 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"public/form.html"))
 });
 
-// app.post("/register", (req, res) => {
-//     const newStudent = {
-//         name: req.body.name,
-//         password: req.body.password
-//     };
+app.post("/register", (req, res) => {
+    const newStudent = {
+        name: req.body.name,
+        password: req.body.password
+    };
 
-//     fs.readFile("students.json", "utf8", (err, data) => {
+    fs.readFile("students.json", "utf8", (err, data) => {
 
-//         let students = [];
+        let students = [];
 
-//         if (!err && data.length > 0) {
-//             students = JSON.parse(data);
-//         }
+        if (!err && data.length > 0) {
+            students = JSON.parse(data);
+        }
 
-//         students.push(newStudent);
+        students.push(newStudent);
 
   
-//         fs.writeFile("students.json", JSON.stringify(students, null, 2), (err) => {
-//             if (err) {
-//                 return res.send("Error saving data");
-//             }
+        fs.writeFile("students.json", JSON.stringify(students, null, 2), (err) => {
+            if (err) {
+                return res.send("Error saving data");
+            }
 
-//             res.send("Student Registered");
-//         });
-//     });
-// });
-
-
-app.post("/register",(req,res)=>{
-    const newStudent = {
-        name : req.body.name,
-        password : req.body.password
-    }
-
-    const data = fs.readFileSync("Students.json","utf8");
-    let students = [];
-    if(data.length>0){
-    students = JSON.parse(data);
-    }
-    students.push(newStudent);
-    fs.writeFileSync("Students.json",JSON.stringify(students, null, 2));
-    // fs.appendFileSync("Students.json", JSON.stringify(newStudent) + "\n"); is tareeke se objext ke form mai append hoyega
-
-res.send("Student Registered");
-
+            res.send("Student Registered");
+        });
+    });
 });
+
+
+
+
+// app.post("/register",(req,res)=>{
+//     const newStudent = {
+//         name : req.body.name,
+//         password : req.body.password
+//     }
+
+//     const data = fs.readFileSync("Students.json","utf8");
+//     let students = [];
+//     if(data.length>0){
+//     students = JSON.parse(data);
+//     }
+//     students.push(newStudent);
+//     fs.writeFileSync("Students.json",JSON.stringify(students, null, 2));
+//     // fs.appendFileSync("Students.json", JSON.stringify(newStudent) + "\n"); is tareeke se objext ke form mai append hoyega
+
+// res.send("Student Registered");
+
+// });
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
